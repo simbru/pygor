8fe78ee182ec9e166ad4c83ea807e9641f085af3
Simen Bruøygard <simen.bruoy@gmail.com>
2024-05-07 20:57:00 +0100

    Split clutsering funcs for future reconstruction into STRF objects (will require re-write probably)

---
+++ src/pygor/strf/clustering/reconstruct.py
@@ -0,0 +1,61 @@
+# Kudos: https://github.com/OpenClassrooms-Student-Center/Multivariate-Exploratory-Analysis/blob/master/functions.py
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+import numpy as np
+import pandas as pd
+import copy
+from scipy.cluster.hierarchy import dendrogram
+from pandas.plotting import parallel_coordinates
+import seaborn as sns
+from sklearn.cluster import KMeans, OPTICS, DBSCAN, AgglomerativeClustering
+from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
+from sklearn.decomposition import PCA
+# Import the sklearn function
+from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler, MaxAbsScaler
+#from collections import Iterable
+import warnings
+#from collections.abs import Iterable
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline#, make_pipeline
+import pygor.filehandling as filehandling
+import pygor.strf.analyse 
+
+
+
+def reconstruct_cluster_spatial(clust_df, cluster_id_str):
+    r_avg, g_avg, b_avg, uv_avg = [], [], [], []
+    for roi, obj in clust_df.query(f"cluster_id == '{cluster_id_str}'")[["roi", "strf_obj"]].to_numpy():
+        r, g, b, uv = pygor.utilities.multicolour_reshape(obj.collapse_times(spatial_centre=True), 4)[:, roi]
+        r_avg.append(r)
+        g_avg.append(g)
+        b_avg.append(b)
+        uv_avg.append(uv)
+    r_avg = np.average(r_avg, axis = 0)
+    g_avg = np.average(g_avg, axis = 0)
+    b_avg = np.average(b_avg, axis = 0)
+    uv_avg = np.average(uv_avg, axis = 0)
+    srf_avgs = np.array([r_avg, g_avg, b_avg, uv_avg])
+    return srf_avgs
+
+
+def reconstruct_cluster_strf(clust_df, cluster_id_str):
+    r_strf, g_strf, b_strf, uv_strf = [], [], [], []
+    for roi, obj in clust_df.query(f"cluster_id == '{cluster_id_str}'")[["roi", "strf_obj"]].to_numpy():
+        centred_strfs = obj.centre_strfs()
+        r, g, b, uv = pygor.utilities.multicolour_reshape(centred_strfs, 4)[:, roi]
+        r_strf.append(r)
+        g_strf.append(g)
+        b_strf.append(b)
+        uv_strf.append(uv)
+    r_strf = np.average(r_strf, axis = 0)
+    g_strf = np.average(g_strf, axis = 0)
+    b_strf = np.average(b_strf, axis = 0)
+    uv_strf =np.average(uv_strf, axis = 0)
+    strf_avgs = np.array([r_strf, g_strf, b_strf, uv_strf])
+    return strf_avgs
+
+
+
+# import build_strf_from_cluster(clust_df):
+# 
