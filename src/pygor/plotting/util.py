d39ccdaa1fb6758e4216f84dc31bcc6d350edae5
Simen Bruøygard <simen.bruoy@gmail.com>
2024-05-28 19:12:47 +0100

    Better scalebar

--- src/pygor/plotting/util.py
+++ src/pygor/plotting/util.py
@@ -1,57 +1,48 @@
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
 def rotate(p, origin=(0, 0), degrees=0):
     angle = np.deg2rad(degrees)
     R = np.array([[np.cos(angle), -np.sin(angle)],
-                [np.sin(angle),  np.cos(angle)]])
+                  [np.sin(angle),  np.cos(angle)]])
     o = np.atleast_2d(origin)
     p = np.atleast_2d(p)
-    return np.squeeze((R @ (p.T-o.T) + o.T).T)
+    return np.squeeze((R @ (p.T - o.T) + o.T).T)
 
-def add_scalebar(x, y, length, ax = None, string = None, text_align = "mid", orientation = 'v', rotation = 0):
-    if ax == None:
+def add_scalebar(x, y, length, ax=None, string=None, text_align="mid", orientation='v', rotation=0):
+    if ax is None:
         ax = plt.gca()
+        ax.set_clip_on(True)
+
     line_width = 8
     text_size = 15
-    start = np.array([x + 1, y + 1])
-    stop = np.array([x + 1, y + 1 + length]).T
-    points=np.array([start, stop])
-    rotation = rotation * -1
-    if orientation == 'v':
-        midpoint = ((start[0] + stop[0]) / 2, (start[1] + stop[1]) / 2)
-        points = np.array(rotate(points, origin=midpoint, degrees=0 + rotation))
-        line = matplotlib.lines.Line2D(points[:, 0], points[:, 1], c = 'k', lw = line_width)
-        line = matplotlib.lines.Line2D(points[:, 0], points[:, 1], c = 'k', lw = line_width)
-        mapping = {
-            "close" : line.get_ydata()[0],
-            "mid"   : np.sum(line.get_ydata()) / 2,
-            "far"   : line.get_ydata()[-1],}
-        text_x = (line.get_xdata()[0] + line.get_xdata()[0] * .1)
-        text_y = mapping[text_align]
-        text_points = np.array([text_x, text_y])
-        print(text_points)
-        #text_points = np.array(rotate(text_points, origin=midpoint, degrees = rotation))
-        print(text_points)
-        #test_x, test_y = new_points[:, 0], new_points[:, 1]
-        ax.text(text_points[0], text_points[1], string, ha = "center", va = "center", size = text_size, rotation = 90 + rotation)
-    if orientation == 'h':
-        # # Calculate the midpoint of the line for the origin of rotation
-        # midpoint = ((start[0] + stop[0]) / 2, (start[1] + stop[1]) / 2)
-        midpoint = start
-        # # Rotate the points
-        points = np.array(rotate(points, origin=midpoint, degrees=90 + rotation))
-        line = matplotlib.lines.Line2D(points[:, 0], points[:, 1], c = 'k', lw = line_width)
-        mapping = {
-            "close" : line.get_ydata()[0],
-            "mid"   : np.sum(line.get_ydata()) / 2,
-            "far"   : line.get_ydata()[-1],}
-        ax.text((line.get_xdata()[0]) - (text_size + line_width) * np.cos(rotation), mapping[text_align] + (line_width) * np.cos(rotation), 
-                string, ha = "center", va = "center", size = text_size, rotation = 0 + rotation)
-    line.set_clip_on(False)
 
-    ax.add_line(line)
+    start = np.array([x, y])
+    if orientation == 'v':
+        stop = np.array([x, y + length])
+    else:  # 'h'
+        stop = np.array([x + length, y])
+
+    points = np.array([start, stop])
+
+    midpoint = np.mean(points, axis=0)
+    points = rotate(points, origin=midpoint, degrees=rotation)
     
-    
+    line = plt.Line2D(points[:, 0], points[:, 1], color='k', linewidth=line_width,
+                    clip_on=False, clip_box = False, mew = 0, solid_capstyle = "butt")
+    ax.add_artist(line)
+
+    if orientation == 'v':
+        text_x = points[0, 0] - 0.05 * (ax.get_xlim()[1] - ax.get_xlim()[0])
+        text_y = {'close': points[0, 1], 'mid': midpoint[1], 'far': points[1, 1]}[text_align]
+        if rotation == 180:
+            text_x = points[0, 0] + 0.05 * (ax.get_xlim()[1] - ax.get_xlim()[0])
+        rotation_angle = 90 + rotation
+    else:  # 'h'
+        text_x = {'close': points[0, 0], 'mid': midpoint[0], 'far': points[1, 0]}[text_align]
+        text_y = points[0, 1] - 0.05 * (ax.get_ylim()[1] - ax.get_ylim()[0])
+        if rotation == 180:
+            text_y = points[0, 1] + 0.05 * (ax.get_ylim()[1] - ax.get_ylim()[0])
+        rotation_angle = 0 + rotation
+
+    ax.text(text_x, text_y, string, ha='center', va='center', fontsize=text_size, rotation=rotation_angle)
