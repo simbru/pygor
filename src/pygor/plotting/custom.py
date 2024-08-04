c0c39229a15cd18145c4cffaa91d5cc49076b0aa
Simen Bruøygard <simen.bruoy@gmail.com>
2024-08-04 12:36:15 +0100

    Adjust colour maps and add annotation label function

--- src/pygor/plotting/custom.py
+++ src/pygor/plotting/custom.py
@@ -12,13 +12,13 @@ all_hues = ["R", "G", "B", "UV", "BW", "BWnoUV"]
 rguv_hues = ["R", "G", "UV"]
 nanometers = ["588", "478", "422", "375"]
 fish_palette = ["#ef8e00", "teal","#5600fe", "fuchsia"]
-polarity_palette = ["k", "lightslategray", "lightpink"]
+polarity_palette = ["lightslategray", "k", "tab:olive", "snow"] # [off, on, opp, mix]
 # polarity_palette = ["lightsteelblue", "lightcoral", "lightpink"]
 
-compare_conditions = {2 : ["gainsboro", "tomato"],
-                    3 : ["gainsboro", "tomato", "darkblue"],
-                    4 : ["gainsboro", "tomato", "darkblue", "purple"],
-                    5 : ["grgainsboroay", "tomato", "darkblue", "purple", "darkgreen"]}
+compare_conditions = {2 : ["grey", "tomato"],
+                3 : ["grey", "tomato", "darkblue"],
+                4 : ["grey", "tomato", "darkblue", "purple"],
+                5 : ["grey", "tomato", "darkblue", "purple", "darkgreen"]}
 
 # red_map = matplotlib.colors.LinearSegmentedColormap.from_list("", ["dimgrey", "grey", "white","#ef8e00","darkred"])
 # green_map = matplotlib.colors.LinearSegmentedColormap.from_list("", ["dimgrey", "grey","white","mediumaquamarine","teal"])
@@ -40,12 +40,55 @@ violet_map = matplotlib.colors.LinearSegmentedColormap.from_list("", ["black", "
 
 maps_concat = [red_map, green_map, blue_map, violet_map]
 
-def label_ax_colour(ax, x = 0.1, y = .9, marker = 'o', 
-            colour = fish_palette[0]):
-    fig = ax.get_figure()
-    bbox = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-    width, height = bbox.width, bbox.height
-    width *= fig.dpi
-    height *= fig.dpi
-    plotarea = width * height
-    ax.scatter(x, y, marker = marker, c = colour, s = plotarea/100, transform = ax.transAxes)
+def label_ax_colour(ax, x = 0.1, y = .9, marker = 'o', colour = fish_palette[0], relative_axis = True, **kwargs):
+        '''The function `label_ax_colour` labels a point on an axis with a marker of a specified color and
+        size, allowing for customization of the position and appearance.
+        
+        Parameters
+        ----------
+        ax
+                The `ax` parameter in the `label_ax_colour` function is the axis object on which you want to plot
+        the marker. It is typically obtained by calling `plt.gca()` in matplotlib or by creating a subplot
+        using `plt.subplots()`.
+        x
+                The `x` parameter in the `label_ax_colour` function is used to specify the x-coordinate where the
+        marker will be placed on the plot.
+        y
+                The `y` parameter in the `label_ax_colour` function is used to specify the y-coordinate of the
+        point where the marker will be placed on the plot. It determines the vertical position of the marker
+        within the plot area.
+        marker, optional
+                The `marker` parameter in the `label_ax_colour` function is used to specify the marker style for
+        the scatter plot. In this function, the `marker` parameter is set to 'o' by default, which
+        represents a circle marker. However, you can customize this parameter by passing different marker
+        colour
+                The `colour` parameter in the `label_ax_colour` function is used to specify the color of the marker
+        that will be plotted on the axis. It is set to `fish_palette[0]` by default, which suggests that it
+        is likely a predefined color from a palette named `fish_palette
+        relative_axis, optional
+                The `relative_axis` parameter in the `label_ax_colour` function determines the coordinate system in
+        which the marker will be placed. It can take on the following values:
+        
+
+        **kwargs are passed to the `ax.scatter` function
+        '''
+        fig = ax.get_figure()
+        bbox = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+        if 's' not in kwargs or 'size' not in kwargs:
+                width, height = bbox.width, bbox.height
+                width *= fig.dpi
+                height *= fig.dpi
+                kwargs["s"] = (width * height) / 100
+        if relative_axis is True:
+                transform_axis = ax.transAxes
+        if relative_axis is False:
+                transform_axis = ax.transData
+        if relative_axis == 'x':
+                transform_axis = ax.get_yaxis_transform()
+        if relative_axis == 'y':
+                transform_axis = ax.get_xaxis_transform()
+        if relative_axis == "fig":
+                transform_axis = ax.transFigure
+        else: 
+                AttributeError("relative_axis must be bool or str 'x', 'y' or 'fig'")    
+        ax.scatter(x, y, marker = marker, c = colour, transform = transform_axis, **kwargs)
