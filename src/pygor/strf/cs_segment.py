ce824cada9140926ffdbe227f9d090670c8421e4
Simen Bruøygard <simen.bruoy@gmail.com>
2024-11-27 12:50:45 +0000

    Add upscale param

--- src/pygor/strf/cs_segment.py
+++ src/pygor/strf/cs_segment.py
@@ -15,13 +15,22 @@ def custom_agglom(
     smooth_times=True,
     kernel=None,
     centre_on_zero=True,
-    island_size_min = 5,
+    upscale=True,
+    island_size_min=5,
     **kwargs,
 ):
     original_shape = inputdata.shape
     inputdata_reshaped = inputdata.reshape(original_shape[0], -1)
     fit_on = inputdata_reshaped.T
     # predict_on = inputdata_reshaped.T
+
+    if upscale is not None:
+        times_flat = fit_on.flatten()
+        new_len = np.prod(fit_on.shape) * upscale
+        upscaled = np.interp(
+            np.arange(0, new_len), np.linspace(0, new_len, len(times_flat)), times_flat
+        ).reshape(fit_on.shape[0], -1)
+        fit_on = upscaled
 
     fit_on_shape = fit_on.shape
     if smooth_times is True:
@@ -56,6 +65,7 @@ def custom_agglom(
         original_shape[1], original_shape[2]
     )  # 1d -> 2d shape
     num_clusts = len(np.unique(initial_prediction_map))
+
     # Generate timecourses for each cluster
     initial_masks = [
         np.repeat(
@@ -84,7 +94,10 @@ def custom_agglom(
             [
                 np.where(
                     skimage.morphology.remove_small_objects(
-                        skimage.morphology.remove_small_holes(prediction_map == i, island_size_min), island_size_min
+                        skimage.morphology.remove_small_holes(
+                            prediction_map == i, island_size_min
+                        ),
+                        island_size_min,
                     ),
                     i,
                     np.nan,
@@ -94,7 +107,78 @@ def custom_agglom(
         ),
         axis=0,
     )
+    if plot is True:
+        prediction_times = extract_times(prediction_map, inputdata)
+        # Store cluster centers
+        # centres = clustering.cluster_centers_
+        fig, ax = plt.subplots(1, 7, figsize = (20, 2))
+        colormap = plt.cm.tab10  # Use the entire Set1 colormap
+        cmap = plt.cm.colors.ListedColormap([colormap(i) for i in range(num_clusts)])
+        space_repr = pygor.strf.spatial.collapse_3d(inputdata)
+        ax[0].imshow(space_repr)
+        ax[1].plot(inputdata_reshaped, alpha = 0.05, c = "black")
+        ax[2].plot(fit_on.T, alpha = 0.05, c = "black")
+        top_3 = np.argsort(np.std(prediction_times, axis = 1))[-2:]
+        ax[4].plot(prediction_times[top_3].T)
+        if smooth_times is not False:
+            ax[3].plot(kernel[0])
+        ax[5].plot(prediction_times.T)
+        ax[6].imshow(pygor.strf.spatial.collapse_3d(inputdata), cmap = "Greys_r")
+        ax[6].imshow(prediction_map, cmap = cmap, alpha = 0.45)
+        titles = ["Space_collapse", "Raw", "Convolved", "Kernel", "MaxVarClusts", "AllClusts", "ClustSpatial"]
+        for n, i in enumerate(ax):
+            i.set_title(titles[n])
     return prediction_map
+
+
+def get_corrcoef_combinations_optimized_with_labels(
+    times, similarity_thresh=0.75, debug_return=False
+):
+    traces_correlation = np.corrcoef(times)
+    np.fill_diagonal(traces_correlation, np.nan)
+    # Get indices of pairs exceeding the threshold
+    upper_triangle_indices = np.triu_indices_from(traces_correlation, k=1)
+    row_indices, col_indices = upper_triangle_indices[0], upper_triangle_indices[1]
+    exceed_indices = np.where(
+        traces_correlation[upper_triangle_indices] > similarity_thresh
+    )
+    similar_pairs = np.array(
+        list(zip(row_indices[exceed_indices], col_indices[exceed_indices]))
+    )
+    if not similar_pairs.any():
+        return times, None, None
+    # Construct adjacency graph and find connected components
+    n = traces_correlation.shape[0]
+    adj_matrix = np.zeros((n, n), dtype=bool)
+    adj_matrix[similar_pairs[:, 0], similar_pairs[:, 1]] = True
+    adj_matrix |= adj_matrix.T  # Symmetric graph
+    labels = np.zeros(n, dtype=int) - 1  # -1 indicates unvisited
+
+    current_label = 0
+    label_changes = {}  # Track how old labels map to new ones
+    for node in range(n):
+        if labels[node] == -1:
+            # Perform a DFS/BFS to assign all connected nodes the same label
+            stack = [node]
+            while stack:
+                curr = stack.pop()
+                if labels[curr] == -1:
+                    labels[curr] = current_label
+                    neighbors = np.where(adj_matrix[curr])[0]
+                    stack.extend(neighbors)
+            # Record how this component is labeled
+            label_changes[current_label] = np.where(labels == current_label)[0]
+            current_label += 1
+
+    # Vectorized averaging without sorting
+    unique_labels = np.unique(labels)
+    merged_traces = np.array(
+        [times[labels == label].mean(axis=0) for label in unique_labels]
+    )
+    if debug_return is True:
+        return merged_traces, labels, label_changes
+    else:
+        return merged_traces
 
 
 def extract_times(prediction_map, inputdata):
@@ -122,27 +206,8 @@ def extract_times(prediction_map, inputdata):
     return prediction_times
 
 
-# def cs_segment(plot = False):
+def cs_segment(plot = False):
 
-#     if plot is True:
-#         # Store cluster centers
-#         # centres = clustering.cluster_centers_
-#         fig, ax = plt.subplots(1, 7, figsize = (20, 2))
-#         colormap = plt.cm.tab10  # Use the entire Set1 colormap
-#         cmap = plt.cm.colors.ListedColormap([colormap(i) for i in range(num_clusts)])
-#         space_repr = pygor.strf.spatial.collapse_3d(inputdata)
-#         ax[0].imshow(space_repr)
-#         ax[1].plot(inputdata_reshaped, alpha = 0.05, c = "black")
-#         ax[2].plot(fit_on.T, alpha = 0.05, c = "black")
-#         top_3 = np.argsort(np.std(prediction_times, axis = 1))[-2:]
-#         ax[4].plot(prediction_times[top_3].T)
-#         if smooth_times is not False:
-#             ax[3].plot(kernel[0])
-#         ax[5].plot(prediction_times.T)
-#         ax[6].imshow(pygor.strf.spatial.collapse_3d(inputdata), cmap = "Greys_r")
-#         ax[6].imshow(prediction_map, cmap = cmap, alpha = 0.45)
-#         titles = ["Space_collapse", "Raw", "Convolved", "Kernel", "MaxVarClusts", "AllClusts", "ClustSpatial"]
-#         for n, i in enumerate(ax):
-#             i.set_title(titles[n])
-#         # plt.show()
-#     pass
+
+        # plt.show()
+    pass
