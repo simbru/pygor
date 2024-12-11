feae3dc5ad33c45c79f71edf5897e7e923f94e47
Simen Bruøygard <simen.bruoy@gmail.com>
2024-12-11 15:58:50 +0000

    maxabs axis param now optional

--- src/pygor/np_ext.py
+++ src/pygor/np_ext.py
@@ -3,9 +3,12 @@ Functions that extend Numpy functionality
 """
 import numpy as np
 
-def maxabs(x, axis):
-    # Get the indices of the maximum absolute values along the specified axis
-    idx = np.ma.argmax(np.abs(x), axis=axis, keepdims=True)
-    # Use np.take_along_axis to get the original values at those indices
-    return np.take_along_axis(x, idx, axis=axis).squeeze(axis)
-
+def maxabs(x, axis = None):
+    if axis is None:
+        idx = np.ma.argmax(np.abs(x))
+        return x.flat[idx]
+    else: 
+        # Get the indices of the maximum absolute values along the specified axis
+        idx = np.ma.argmax(np.abs(x), axis=axis, keepdims=True)
+        # Use np.take_along_axis to get the original values at those indices
+        return np.take_along_axis(x, idx, axis=axis).squeeze(axis)
