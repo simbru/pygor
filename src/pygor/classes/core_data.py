6363a4e20d6180ed91c4ec0b03a79d8ba2a71ad0
Simen Bruøygard <simen.bruoy@gmail.com>
2025-02-15 15:04:33 +0000

    Improve core_data averages plotting

--- src/pygor/classes/core_data.py
+++ src/pygor/classes/core_data.py
@@ -387,7 +387,13 @@ class Core:
         ax.imshow(pygor.utilities.min_max_norm(d3, 0, 1))
 
     def plot_averages(
-        self, rois=None, figsize=(None, None), figsize_scale=None, axs=None, **kwargs
+        self, rois=None, 
+        figsize=(None, None), 
+        figsize_scale=None, 
+        axs=None, 
+        independent_scale = False, 
+        n_rois_raster = 50,
+        **kwargs,
     ):
         """
         A function to plot the averages of specified regions of interest (rois) on separate subplots within a figure.
@@ -421,12 +427,8 @@ class Core:
             raise AttributeError(
                 "self.averages is nan, meaning it has likely not been generated"
             )
-        if (
-            isinstance(rois, Iterable) is False and not rois
-        ):  # I dont like this solution, but it gets around user ambigouity error if passing numpy array
+        if rois is None:
             rois = np.arange(0, self.num_rois)
-            # fig, axs = plt.subplots(self.num_rois, figsize = figsize, sharey=True, sharex=True)
-            # ^ no longer needed, since error handling of 'rois' leads to naturally solving this
         if isinstance(rois, Iterable) is False:
             rois = [rois]
         if isinstance(rois, np.ndarray) is False:
@@ -447,43 +449,77 @@ class Core:
             )
             rois = rois[filter_result]
         if figsize == (None, None):
-            figsize = (10, len(rois))
+            figsize = (5, len(rois))
         if figsize_scale is not None:
             figsize = np.array(figsize) * np.array(figsize_scale)
-        # Generate matplotlib plot
-        colormap = plt.cm.jet_r(np.linspace(1, 0, self.num_rois))
-        if axs is None:
-            fig, axs = plt.subplots(
-                len(rois), figsize=figsize, sharey=True, sharex=True
-            )
+        if len(rois) < n_rois_raster:
+            # Generate matplotlib plot
+            colormap = plt.cm.jet_r(np.linspace(1, 0, len(rois)))
+            if axs is None:
+                fig, axs = plt.subplots(
+                    len(rois), figsize=figsize, sharey=True, sharex=True
+                )
+            else:
+                fig = plt.gcf()
+            # Loop through and plot wwithin axes
+            if len(
+                rois == 1
+            ):  # This takes care of passing just 1 roi, not breaking axs.flat in the next line
+                axs = np.array([axs])
+            sd_ratio_scalebar = 0.5
+            phase_dur = self.ms_dur / self.phase_num
+            for n, (ax, roi, label) in enumerate(zip(axs.flat, rois, roi_labels)):
+                ax.plot(self.snippets[roi].T, c="grey", alpha=0.5)
+                ax.plot(self.averages[roi], color=colormap[n])
+                ax.set_xlim(0, len(self.averages[0]))
+                if independent_scale is False:
+                    ax.set_ylim(np.min(self.snippets[rois]), np.max(self.snippets[rois]))
+                else:
+                    closest_sd = np.ceil(np.max(self.averages[roi])*sd_ratio_scalebar)
+                    pygor.plotting.add_scalebar(closest_sd, string = f"{closest_sd.astype(int)} SD",ax=ax, flip_text=True, x=1.015, y = 0.1)
+                ax.set_yticklabels([])
+                ax.set_ylabel(label, rotation=0, verticalalignment="center")
+                ax.spines[["top", "bottom", "right"]].set_visible(False)
+                # Now we need to add axvspans (don't think I can avoid for loops inside for loops...)
+                if self.phase_num != 1:
+                    for interval in range(self.phase_num)[::2]:
+                        ax.axvspan(
+                            interval * phase_dur,
+                            (interval + 1) * phase_dur,
+                            alpha=0.2,
+                            color="gray",
+                            lw=0,
+                        )
+                ax.grid(False)
+            if independent_scale is False:
+                closest_sd = np.ceil(np.max(self.averages[rois])*sd_ratio_scalebar)
+                pygor.plotting.add_scalebar(closest_sd, string = f"{closest_sd.astype(int)} SD",ax=axs.flat[-1], flip_text=True, x=1.015, y = 0.1)
+            ax.set_xlabel("Time (ms)")
+            fig.subplots_adjust(wspace=0, hspace=0)
         else:
-            fig = plt.gcf()
-        # Loop through and plot wwithin axes
-        if len(
-            rois == 1
-        ):  # This takes care of passing just 1 roi, not breaking axs.flat in the next line
-            axs = np.array([axs])
-        phase_dur = self.ms_dur / self.phase_num
-        for ax, roi, label in zip(axs.flat, rois, roi_labels):
-            ax.plot(self.snippets[roi].T, c="grey", alpha=0.5)
-            ax.plot(self.averages[roi], color=colormap[roi])
+            avg_epoch_dur = np.average(np.diff(self.triggertimes.reshape(-1, self.trigger_mode)[:, 0]))
+            epoch_reshape = self.triggertimes.reshape(-1, self.trigger_mode)
+            temp_arr = np.empty(epoch_reshape.shape)
+            for n, i in enumerate(epoch_reshape):
+                temp_arr[n] = i - (avg_epoch_dur * n)
+            avg_epoch_triggertimes = np.average(temp_arr, axis=0)
+            markers_arr = avg_epoch_triggertimes * (1 / self.linedur_s)
+            markers_arr -= markers_arr[0]
+            fig, ax = plt.subplots(1,)
+            # scaler = sklearn.preprocessing.MaxAbsScaler()
+            arr = self.averages
+            # arr = scaler.fit_transform(arr.T).T
+            img = ax.imshow(arr, aspect="auto", cmap = "Greys_r")
+            # ax.set_xticklabels(np.round(load.triggertimes, 3))
+            for i in markers_arr:
+                ax.axvline(x=i, color="r", alpha=0.5)
+                # ax.axvline(x=i + (avg_epoch_dur * (1 / load.linedur_s)/load.trigger_mode)/2, color="blue", alpha=0.5)
+            plt.colorbar(img)
+            # ax.set_xticklabels(np.round(self.triggertimes - self.triggertimes[0], 2) )
+            # print(ax.xaxis.get_majorticklocs())
+            ax.set_xticks(np.ceil(ax.xaxis.get_majorticklocs()), np.ceil(ax.xaxis.get_majorticklocs() / 1000))
             ax.set_xlim(0, len(self.averages[0]))
-            ax.set_yticklabels([])
-            ax.set_ylabel(label, rotation=0, verticalalignment="center")
-            ax.spines[["top", "bottom"]].set_visible(False)
-            # Now we need to add axvspans (don't think I can avoid for loops inside for loops...)
-            if self.phase_num != 1:
-                for interval in range(self.phase_num)[::2]:
-                    ax.axvspan(
-                        interval * phase_dur,
-                        (interval + 1) * phase_dur,
-                        alpha=0.2,
-                        color="gray",
-                        lw=0,
-                    )
-            ax.grid(False)
-        ax.set_xlabel("Time (ms)")
-        fig.subplots_adjust(wspace=0, hspace=0)
+            # ax.set_xticklabels(np.array([int(i) for i in ax.get_xticklabels()]) / 1000)
         # plt.tight_layout()
         return fig, axs
 
