5a025c69ee5f2e54fe97ee1421ef1d10061bdd25
Michael Forsthofer <m.forsthofer@outlook.de>
2025-03-07 17:24:07 +0000

    turned average trigger calculation into a method, that is then called in the mixed_stimuli object

--- src/pygor/classes/mixed_objects_data.py
+++ src/pygor/classes/mixed_objects_data.py
@@ -1,8 +1,16 @@
 from dataclasses import dataclass
 from pygor.classes.core_data import Core
 import numpy as np
+import pygor.timeseries.mixed_stimuli.methods
 
 @dataclass(kw_only=True, repr=False)
 class MixedObjects(Core):
     stimtypes: np.ndarray = np.nan
+
+    def __post_init__(self):
+        super().__post_init__()
+
+    def mean_triggertimes_ms(self):
+        return pygor.timeseries.mixed_stimuli.methods.mean_triggertimes_ms(self)
+
 
---
+++ src/pygor/timeseries/mixed_stimuli/methods.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def mean_triggertimes_ms(self):
+        triggertimes_ms = (self.triggertimes_frame-self.triggertimes_frame[0])/self.frame_hz/self.linedur_s*self.n_planes 
+        triggertimes_ms_reshaped = triggertimes_ms[:-1].reshape(-1, self.trigger_mode)
+        mean_triggertimes = np.mean(triggertimes_ms_reshaped, axis=0)-np.mean(triggertimes_ms_reshaped, axis=0)[0]
+        return mean_triggertimes
